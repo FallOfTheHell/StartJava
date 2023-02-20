4822a0e05d12b612e56bc83a5a08327e8b2aa2f4
FallOfTheHell <gleb.epifancev@mail.ru>
2023-02-20 21:01:53 +0700

    Недочет в классе GuessNumber

--- leasson1/GuessNumber.java
+++ leasson1/GuessNumber.java
@@ -3,20 +3,20 @@ package leasson1;
 public class GuessNumber {
     public static void main(String[] args) {
         int player;
-        int pc = (int) (Math.random() * 100) + 1;
+        int computer = (int) (Math.random() * 101);
         int tryCount = 0;
 
         do {
             tryCount++;
-            player = (int) (Math.random() * 100) + 1;
+            player = (int) (Math.random() * 101);
             System.out.println("Игрок: " + player);
-            System.out.println("компьютер: " + pc);
-            if (player > pc) {
+            System.out.println("компьютер: " + computer);
+            if (player > computer) {
                 System.out.println("Число: " + player + " больше того, что загадал компьютер");
-            } else if (player < pc) {
+            } else if (player < computer) {
                 System.out.println("Число: " + player + " меньше того, что загадал компьютер");
             }
-        } while (player != pc);
+        } while (player != computer);
         System.out.println("Вы победили!");
         System.out.println("Количество попыток: " + tryCount);
     }
