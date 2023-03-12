ffaa4c2494d7750e89f4f8bfab87457a07fc9bd6
FallOfTheHell <gleb.epifancev@mail.ru>
2023-03-12 18:40:40 +0700

    Глобальное исправление методов

--- src/com/startjava/leasson_1/Calculator.java
+++ src/com/startjava/leasson_1/Calculator.java
@@ -1,4 +1,4 @@
-package com.startjava.leasson_1.final;
+package com.startjava.leasson_1;
 
 public class Calculator {
     public static void main(String[] args) {
--- src/com/startjava/leasson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/leasson_2_3_4/array/ArrayTheme.java
@@ -7,125 +7,139 @@ public class ArrayTheme {
 
     public static void main(String[] args) {
         //1 task
-        reversArray();
+        reverseArrayValues();
 
         //2 task
-        workArray();
+        productArrayElements();
 
         //3 task
-        deleteElement();
+        deleteElements();
 
         //4 task
-        ladderArray();
+        ladderArrayReverse();
 
         //5 task
-        generationUniqueNumber();
+        generationUniqueNumbers();
 
         //6 task
-        copyLines();
+        copyNonEmptyStrings();
     }
 
-    public static void reversArray() {
+    private static void reverseArrayValues() {
         System.out.println("1. Реверс значений массива");
-        int[] reverse = {1, 5, 2, 4, 3, 7, 6};
+        int[] arrayNum = {1, 5, 2, 4, 3, 7, 6};
 
-        System.out.println("До реверса");
-        for (int i = 0; i < reverse.length; i++) {
-            System.out.print(reverse[i] + " ");
+        System.out.println("\nДо модификации");
+        for (int i = arrayNum.length - 1; i >= 0; i--) {
+            System.out.print(arrayNum[i] + " ");
         }
 
-        System.out.println("\nПосле реверса");
-        for (int i = reverse.length - 1; i >= 0; i--) {
-            System.out.print(reverse[i] + " ");
+        System.out.println("\nПосле модификации");
+        for (int i = 0; i < arrayNum.length; i++) {
+            System.out.print(arrayNum[i] + " ");
         }
     }
 
-    public static void workArray() {
+    private static void productArrayElements() {
         System.out.println("\n\n2. Вывод произведения элементов массива");
-        int[] works = new int[10];
-        works[0] = 0;
-        works[1] = 1;
-        works[2] = 2;
-        works[3] = 3;
-        works[4] = 4;
-        works[5] = 5;
-        works[6] = 6;
-        works[7] = 7;
-        works[8] = 8;
-        works[9] = 9;
-        int result = 1;
-        for (int i = 1; i < works.length - 1; i++) {
-            works[i] = i;
-            result *= works[i];
-            System.out.print(works[i] + (i < works.length - 2 ? "*" : "=" + result));
+        int[] productArray = new int[10];
+
+        for (int i = 0; i < productArray.length; i++) {
+            productArray[i] = i;
         }
-        System.out.println("\n" + works[0] + " " + works[9]);
+
+        int result = 1;
+        for (int i = 1; i < productArray.length - 1;i++) {
+            productArray[i] = i;
+            result *= productArray[i];
+            System.out.print(productArray[i] + (i < 8 ? " * " : " = " + result));
+        }
+        System.out.println("\n" + productArray[0] + " " + productArray[9]);
     }
 
-    public static void deleteElement() {
+    private static void deleteElements() {
         System.out.println("\n3. Удаление элементов массива");
 
         Random random = new Random();
 
-        double[] deleteArray = new double[15];
-        for (int i = 0; i < deleteArray.length; i++) {
-            deleteArray[i] = random.nextDouble();
+        double[] array = new double[15];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextDouble();
         }
 
-        for (int i = 0; i < deleteArray.length - 8; i++) {
-            System.out.printf("%10.3f", deleteArray[i]);
+        int middleIndex = array.length / 2;
+
+        System.out.println("Исходный массив:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%8.3f", array[i]);
+            if (i == middleIndex) {
+                System.out.println();
+            }
         }
         System.out.println();
-        int middleIndex = deleteArray.length / 2;
-        for (int i = 7; i < deleteArray.length; i++) {
-            deleteArray[i] = 0;
-            System.out.printf("%10.3f", deleteArray[i]);
+
+        int numZeros = 0;
+
+        System.out.println("\nИзмененный массив:");
+        for (int i = 0; i < array.length; i++) {
+            if (i <= middleIndex) {
+                System.out.printf("%8.3f", array[i]);
+            }else {
+                numZeros++;
+                array[i] = 0;
+                System.out.printf("%8.3f", array[i]);
+            }
+            if (i == middleIndex){
+                System.out.println();
+            }
         }
+
+        System.out.println("\nКоличество обнуленных ячеек: " + numZeros);
     }
 
-    public static void ladderArray() {
-        System.out.println("\n4. Вывод элементов массива лесенкой в обратном порядке");
+    private static void ladderArrayReverse() {
+        System.out.println("\n\n4. Вывод элементов массива лесенкой в обратном порядке");
 
-        char[] letters = new char[26];
-        for (int i = 0; i < letters.length; i++) {
-            letters[i] = (char) ('A' + i);
+        char[] alphabet = new char[26];
+        for (int i = 0; i < alphabet.length; i++) {
+            alphabet[i] = (char) ('A' + i);
         }
 
-        for (int i = letters.length - 1; i >= 0; i--) {
-            for (int j = letters.length - 1; j >= i; j--) {
-                System.out.print(letters[j] + " ");
+        for (int i = alphabet.length - 1; i >= 0; i--) {
+            for (int j = alphabet.length - 1; j >= i; j--) {
+                System.out.print(alphabet[j]);
             }
             System.out.println();
         }
     }
 
-    public static void generationUniqueNumber() {
+    private static void generationUniqueNumbers() {
         System.out.println("\n5. Генерация уникальных чисел");
 
-        int[] randomArray = new int[30];
+        int[] uniqueNum = new int[30];
         int endRange = 0;
 
-        while (endRange < 30) {
-            boolean alreadyExists = false;
+        while (endRange < uniqueNum.length) {
+            boolean unique = false;
             int numRandom = (int) (Math.random() * 40) + 60;
 
             for (int i = 0; i < endRange; i++) {
-                if (randomArray[i] == numRandom) {
-                    alreadyExists = true;
+                if (uniqueNum[i] == numRandom) {
+                    unique = true;
                     break;
                 }
             }
 
-            if (!alreadyExists) {
-                randomArray[endRange] = numRandom;
+            if (!unique) {
+                uniqueNum[endRange] = numRandom;
                 endRange++;
             }
         }
 
         int count = 1;
-        Arrays.sort(randomArray);
-        for (int i = 0; i < randomArray.length; i++) {
-            System.out.printf("%3d ", randomArray[i]);
+        Arrays.sort(uniqueNum);
+        for (int i = 0; i < uniqueNum.length; i++) {
+            System.out.printf("%3d ", uniqueNum[i]);
             count++;
             if (count == 11) {
                 System.out.println();
@@ -134,24 +148,21 @@ public class ArrayTheme {
         }
     }
 
-    public static void copyLines() {
+    private static void copyNonEmptyStrings() {
         System.out.println("\n6. Копирование не пустых строк");
-        String[] linesArray = {"    ", "AA", "", "BBB", "CC", "D", "    ", "E", "FF", "G", ""};
-        System.out.println(Arrays.toString(linesArray));
-        String[] copyArray = new String[linesArray.length];
-        int count = 0;
+        String[] srcStrings = {"    ", "AA", "", "BBB", "CC", "D", "    ", "E", "FF", "G", ""};
+        System.out.println(Arrays.toString(srcStrings));
 
-        //TODO: в 147 строке какую запись лучше использовать?
-        // "s.strip().length() > 0" или "!s.isBlank"?
-        for (String s : linesArray) {
+        int count = 0;
+        for (String s : srcStrings) {
             if (!s.isBlank()) {
-                copyArray[count++] = s;
+                srcStrings[count++] = s;
             }
         }
 
-        String[] result = new String[copyArray.length];
-        System.arraycopy(copyArray, 0, result, 0, count);
-        System.out.println(Arrays.toString(result));
+        String[] destStrings = new String[srcStrings.length];
+        System.arraycopy(srcStrings, 0, destStrings, 0, count);
+        System.out.println(Arrays.toString(destStrings));
     }
 }
 
