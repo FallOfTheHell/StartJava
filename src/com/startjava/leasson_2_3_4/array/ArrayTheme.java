be28ad5d720b30788cdfa72f75ada616996a98eb
FallOfTheHell <gleb.epifancev@mail.ru>
2023-03-14 14:43:18 +0700

    Допиливание недочетов

--- src/com/startjava/leasson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/leasson_2_3_4/array/ArrayTheme.java
@@ -19,7 +19,7 @@ public class ArrayTheme {
         ladderArrayReverse();
 
         //5 task
-        generationUniqueNumbers();
+        generateUniqueNumbers();
 
         //6 task
         copyNonEmptyStrings();
@@ -28,115 +28,108 @@ public class ArrayTheme {
     private static void reverseArrayValues() {
         System.out.println("1. Реверс значений массива");
         int[] intsArray = {1, 5, 2, 4, 3, 7, 6};
+        int length = intsArray.length;
 
-        System.out.println("\nДо модификации");
-        System.out.println(Arrays.toString(intsArray));
+        System.out.println("\nДо модификации\n" + Arrays.toString(intsArray));
 
-        for (int i = 0; i < intsArray.length / 2; i++) {
+        for (int i = 0; i < length / 2; i++) {
             int temp = intsArray[i];
-            intsArray[i] = intsArray[intsArray.length - 1 - i];
-            intsArray[intsArray.length - 1 - i] = temp;
+            intsArray[i] = intsArray[length - i - 1];
+            intsArray[length - i - 1] = temp;
         }
+        //TODO: Если честно, у меня даже идей нет как можно сделать код проще,
+        // "упрости код так, чтобы вместо intsArray.length - 1 - i было intsArray.length",
+        // можно пожалуйста пару подсказок что нужно сделать))
 
-        System.out.println("\nПосле модификации");
-        System.out.println(Arrays.toString(intsArray));
+        System.out.println("\nПосле модификации\n" + Arrays.toString(intsArray));
     }
 
     private static void productArrayElements() {
-        System.out.println("\n\n2. Вывод произведения элементов массива");
-        int[] productArray = new int[10];
+        System.out.println("\n2. Вывод произведения элементов массива");
+        int[] multipliers = new int[10];
+        int length = multipliers.length;
 
-        for (int i = 0; i < productArray.length; i++) {
-            productArray[i] = i;
+        for (int i = 0; i < length; i++) {
+            multipliers[i] = i;
         }
 
         int result = 1;
-        for (int i = 1; i < productArray.length - 1; i++) {
-            productArray[i] = i;
-            result *= productArray[i];
-            System.out.print(productArray[i] + (i < productArray[productArray.length - 2] ? " * " : " = " + result));
+        for (int i = 1; i < length - 1; i++) {
+            result *= multipliers[i];
+            System.out.print(multipliers[i] + (i < multipliers[length - 2] ? " * " : " = " + result));
         }
-        System.out.println("\n" + productArray[0] + " " + productArray[9]);
+        System.out.println("\n" + multipliers[0] + " " + multipliers[9]);
     }
 
     private static void deleteElements() {
         System.out.println("\n3. Удаление элементов массива");
 
         Random random = new Random();
-
         double[] randomNums = new double[15];
-        for (int i = 0; i < randomNums.length; i++) {
+        int length = randomNums.length;
+        int numZeros = 0;
+
+        for (int i = 0; i < length; i++) {
             randomNums[i] = random.nextDouble();
         }
 
-        int middleIndex = randomNums.length / 2;
+        int middleIndex = length / 2;
         double middleValue = randomNums[middleIndex];
 
         System.out.println("Исходный массив:");
-        for (int i = 0; i < randomNums.length; i++) {
-            System.out.printf("%8.3f", randomNums[i]);
-            if (i == middleIndex) {
-                System.out.println();
-            }
-        }
-        System.out.println();
+        outputNums(randomNums, middleIndex);
 
-        int numZeros = 0;
-
-        //TODO: Я вообще без понятия как изменить этот "метод",
-        // чтобы было правильно,
-        // можно хотя бы пару подсказок как реализовать правильно
         System.out.println("\nИзмененный массив:");
-        for (int i = 0; i < randomNums.length; i++) {
-            if (i <= middleIndex) {
-                System.out.printf("%8.3f", randomNums[i]);
-            } else {
+        for (int i = 0; i < length; i++) {
+            if (randomNums[i] >= middleValue) {
                 numZeros++;
                 randomNums[i] = 0;
-                System.out.printf("%8.3f", randomNums[i]);
-            }
-            if (i == middleIndex) {
-                System.out.println();
             }
         }
-
+        outputNums(randomNums, middleIndex);
 
         System.out.println("\nКоличество обнуленных ячеек: " + numZeros);
     }
 
-    //TODO: Логично ли я назвал метод отвечающий за ввод массива?
-    public static void outputElementArray(double[] arr, int middleIndex) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.printf("%8.3f", arr[i]);
-            if (i == middleIndex) {
+    private static void outputNums(double[] array, int index) {
+        int length = array.length;
+
+        for (int i = 0; i < length; i++) {
+            System.out.printf("%8.3f", array[i]);
+            if (i == index) {
                 System.out.println();
             }
         }
+        System.out.println();
     }
 
     private static void ladderArrayReverse() {
-        System.out.println("\n\n4. Вывод элементов массива лесенкой в обратном порядке");
+        System.out.println("\n4. Вывод элементов массива лесенкой в обратном порядке");
 
         char[] alphabet = new char[26];
-        for (int i = 0; i < alphabet.length; i++) {
+        int length = alphabet.length;
+
+        for (int i = 0; i < length; i++) {
             alphabet[i] = (char) ('A' + i);
         }
 
-        for (int i = alphabet.length - 1; i >= 0; i--) {
-            for (int j = alphabet.length - 1; j >= i; j--) {
+        for (int i = length - 1; i >= 0; i--) {
+            for (int j = length - 1; j >= i; j--) {
                 System.out.print(alphabet[j]);
             }
             System.out.println();
         }
     }
 
-    private static void generationUniqueNumbers() {
+    private static void generateUniqueNumbers() {
         System.out.println("\n5. Генерация уникальных чисел");
 
         int[] uniqueNums = new int[30];
+        int length = uniqueNums.length;
+
         int increasingNum = 0;
 
-        while (increasingNum < uniqueNums.length) {
+        while (increasingNum < length) {
             boolean unique = false;
             int numRandom = (int) (Math.random() * 40) + 60;
 
@@ -155,7 +148,7 @@ public class ArrayTheme {
 
         int count = 1;
         Arrays.sort(uniqueNums);
-        for (int i = 0; i < uniqueNums.length; i++) {
+        for (int i = 0; i < length; i++) {
             System.out.printf("%3d ", uniqueNums[i]);
             count++;
             if (count == 11) {
