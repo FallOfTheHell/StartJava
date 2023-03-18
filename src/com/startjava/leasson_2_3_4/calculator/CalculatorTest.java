afa22a4cad138955ba83649f6efd66785f688f4a
FallOfTheHell <gleb.epifancev@mail.ru>
2023-03-18 21:57:58 +0700

    Добавление рекурсии

--- src/com/startjava/leasson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/leasson_2_3_4/calculator/CalculatorTest.java
@@ -11,19 +11,30 @@ public class CalculatorTest {
         while (!userAnswer.equals("no")) {
             System.out.print("Введите математическое выражение: ");
             double result = calculator.calculate(scanner.nextLine());
-            if (result == 0) {
-                System.out.printf("Результат: %d%n", 0);
-            } else if (Math.floor(result) == result) {
-                System.out.printf("Результат: %d%n", (int) result);
-            } else {
-                System.out.printf("Результат: %.3f%n", result);
-            }
+            printResult(result);
             System.out.print("Хотите продолжить вычисления? [yes/no] ");
-            userAnswer = scanner.nextLine().toLowerCase();
-            if (!userAnswer.equals("yes")) {
-                System.out.print("Пожалуйста, введите 'yes' или 'no': ");
-                userAnswer = scanner.nextLine().toLowerCase();
-            }
+            userAnswer = getUserAnswer(scanner);
+        }
+    }
+
+    private static void printResult(double result){
+        if (result == 0) {
+            System.out.println("Результат: " + 0);
+        } else if (result % 1 == 0) {
+            System.out.printf("Результат: %d%n", (int) result);
+        } else {
+            System.out.printf("Результат: %.3f%n", result);
+        }
+    }
+
+    //TODO: Если было дело не в рекурсии, то тогда я не знаю как решить без вложенного while
+    private static String getUserAnswer(Scanner scanner) {
+        String userAnswer = scanner.nextLine().toLowerCase();
+        if (userAnswer.equals("yes") || userAnswer.equals("no")) {
+            return userAnswer;
+        } else {
+            System.out.print("Пожалуйста, введите 'yes' или 'no': ");
+            return getUserAnswer(scanner);
         }
     }
 }
