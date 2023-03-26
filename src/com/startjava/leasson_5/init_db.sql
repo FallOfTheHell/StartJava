97da3dc28f6b99178301111853ed2c22da707499
FallOfTheHell <gleb.epifancev@mail.ru>
2023-03-26 21:33:25 +0700

    DB правописание

--- src/com/startjava/leasson_5/init_db.sql
+++ src/com/startjava/leasson_5/init_db.sql
@@ -1,12 +1,12 @@
 INSERT INTO Jaegers (modelName, mark, height, weight, status, origin, launch, kaijukill)
 VALUES ('Gipsy Danger', 'Mark-3', 79.25, 1.98, 
-       'Destroyed', 'United States of America', 'July 10, 2017', 9),
-       ('Striker Eureka', 'Mark-5', 76.2, 1.850, 'Destroyed', 'Australia', 'November 2, 2019', 11),
-       ('Cherno Alpha', 'Mark-1', 85.34, 2.412, 'Destroyed', 'Russia', '2015', 6),
-       ('Coyote Tango', 'Mark-1', 85.34, 2.312, 'Destroyed', 'Japan','December 30th, 2015', 2),
-       ('Gipsy Avenger', 'Mark-6', 81.77, 2.004, 'Destroyed', NULL, 'January 12, 2034', 1),
-       ('Crimson Typhoon', 'Mark-4', 76.2, 1.722, 'Destroyed', 'China', 'August 22, 2018', 7),
-       ('Flanker', 'Mark-4', 75.2, 1.798, 'Destroyed', 'Russia', 'August 22, 2020', 4),
-       ('k700', 'Mark-7', 89.2, 2.598, 'Destroyed', 'Kazakstan', 'February 20, 2030', 4),
-       ('Vitec', 'Mark-3', 69.2, 1.598, 'Destroyed', 'Kazakstan', 'February 20, 2025', 2),
-       ('Bumer', 'Mark-2', 73.2, 2.198, 'Destroyed', 'Germany', 'September 11, 2018', 2);
+    'Destroyed', 'United States of America', 'July 10, 2017', 9),
+    ('Striker Eureka', 'Mark-5', 76.2, 1.850, 'Destroyed', 'Australia', 'November 2, 2019', 11),
+    ('Cherno Alpha', 'Mark-1', 85.34, 2.412, 'Destroyed', 'Russia', '2015', 6),
+    ('Coyote Tango', 'Mark-1', 85.34, 2.312, 'Destroyed', 'Japan','December 30th, 2015', 2),
+    ('Gipsy Avenger', 'Mark-6', 81.77, 2.004, 'Destroyed', NULL, 'January 12, 2034', 1),
+    ('Crimson Typhoon', 'Mark-4', 76.2, 1.722, 'Destroyed', 'China', 'August 22, 2018', 7),
+    ('Flanker', 'Mark-4', 75.2, 1.798, 'Destroyed', 'Russia', 'August 22, 2020', 4),
+    ('k700', 'Mark-7', 89.2, 2.598, 'Destroyed', 'Kazakstan', 'February 20, 2030', 4),
+    ('Vitec', 'Mark-3', 69.2, 1.598, 'Destroyed', 'Kazakstan', 'February 20, 2025', 2),
+    ('Bumer', 'Mark-2', 73.2, 2.198, 'Destroyed', 'Germany', 'September 11, 2018', 2);
--- src/com/startjava/leasson_5/queries.sql
+++ src/com/startjava/leasson_5/queries.sql
@@ -10,30 +10,27 @@ SELECT *
 \echo 'отобразите роботов нескольких серий: Mark-1 и Mark-4'
 SELECT * 
   FROM Jaegers 
-  WHERE mark 
-  IN ('Mark-4', 'Mark-1');
+ WHERE mark IN ('Mark-4', 'Mark-1');
 
 \echo 'отобразите всех роботов, кроме Mark-1 и Mark-4'
 SELECT * 
   FROM Jaegers 
-  WHERE mark 
-  NOT IN ('Mark-4', 'Mark-1');
+ WHERE mark NOT IN ('Mark-4', 'Mark-1');
 
 \echo 'отсортируйте таблицу по убыванию по столбцу mark'
 SELECT * 
   FROM Jaegers 
-  ORDER BY mark 
-  DESC;
+  ORDER BY mark DESC;
 
 \echo 'отобразите информацию о самом старом роботе'
 SELECT * 
   FROM Jaegers 
-  WHERE launch = (SElECT MIN(launch) FROM Jaegers);
+ WHERE launch = (SELECT MIN(launch) FROM Jaegers);
 
 \echo 'отобразите роботов, которые уничтожили больше всех kaiju'
 SELECT * 
   FROM Jaegers 
-  WHERE kaijuKill = (SElECT MAX(kaijuKill) FROM Jaegers);
+ WHERE kaijuKill = (SElECT MAX(kaijuKill) FROM Jaegers);
 
 \echo 'отобразите средний вес роботов'
 SELECT AVG(weight) 
@@ -41,9 +38,10 @@ SELECT AVG(weight)
 
 \echo 'увеличьте на единицу количество уничтоженных kaiju у роботов, которые до сих пор не разрушены'
 UPDATE jaegers 
-  SET kaijukill = kaijukill + 1 
-  WHERE status != 'Destroyed';
+   SET kaijukill = kaijukill + 1 
+ WHERE status != 'Destroyed';
 
 \echo 'удалите уничтоженных роботов' 
-DELETE FROM jaegers 
-  WHERE status = 'Destroyed';
+DELETE 
+  FROM jaegers 
+ WHERE status = 'Destroyed';
